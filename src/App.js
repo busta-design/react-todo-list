152d6a7b61ed06bbe4ac3c5f6268b0a558ccb2df
Jose Andres Bustamante Pacheco <andresbusta4444@gmail.com>
2021-01-13 20:33:33 -0400

    Saludo y agregador de tareas

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Bienvenido Andres a tu lista de quehaceres</h1>
+        <form>
+            <input type="text" size="40" placeholder="Add to do..."/>
+            <input type="submit" name="button" value="Add"/>
+        </form>
+        
       </header>
     </div>
   );
